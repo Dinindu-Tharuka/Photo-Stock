622257bcf2befbe45c14b5b392a17da379e884d1
DININDU THARUKA KALUARACHCHI <dinidutharuka@gmail.com>
2023-10-24 11:26:19 +0530

    create nav bar footer

--- src/componants/Navbar/Footer/NavBarFooter.tsx
+++ src/componants/Navbar/Footer/NavBarFooter.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
+import React from "react";
 
 const NavBarFooter = () => {
   return (
-    <div>NavBarFooter</div>
-  )
-}
+      <Tabs width='100%'>
+        <TabList>
+          <Tab>Main</Tab>
+          <Tab>Admin</Tab>
+          <Tab>Photo Grapher</Tab>
+        </TabList>
 
-export default NavBarFooter
+        <TabPanels>
+          <TabPanel>
+            <p>one!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+   
+  );
+};
+
+export default NavBarFooter;
--- src/componants/Navbar/Main/NavBar.tsx
+++ src/componants/Navbar/Main/NavBar.tsx
@@ -1,10 +1,12 @@
 import { VStack } from '@chakra-ui/react'
 import NavBarHeader from '../Header/NavBarHeader'
+import NavBarFooter from '../Footer/NavBarFooter'
 
 const NavBar = () => {
   return (
     <VStack padding={5}>
       <NavBarHeader/>
+      <NavBarFooter/>
     </VStack>
   )
 }
